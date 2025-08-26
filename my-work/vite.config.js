004b5b00b8a6d2187c84960946e5d5dc8f158de3
yeaji.kim <83742940+yeajikim@users.noreply.github.com>
2025-08-26 13:34:32 +0900

    build: vite.config.js 빌드 경로 수정

--- my-work/vite.config.js
+++ my-work/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
   },
   server: {
     open: true,
@@ -17,4 +17,4 @@ export default defineConfig({
       '@components': '/src/components',
     },
   },
-})
+});
